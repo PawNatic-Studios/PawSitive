7839dcec6476bb9c70aa72141197b3577db0dffa
R3aFL3X <dev.reflex@fn.de>
2020-11-11 10:49:18 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import discord
 from discord.ext import commands
+
 import paw_settings
 
-client = commands.Bot(command_prefix = '<')
+client = commands.Bot(command_prefix='!')
 
 #Login check on console
 @client.event
@@ -10,14 +11,13 @@ async def on_ready():
     print('{0.user} has logged in Successesfully!'.format(client))
     print('Version ' + (paw_settings.VERSION)) 
 
-#Test bot active functions on server
+#Test client active functions on server
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
-
-    if message.content.startswith('!paw' + ' ?'):
-        await message.channel.send('PawSitive!')
+    if message.content.startswith('xyc'):
+        await message.channel.send('PawSitive is still running...')
 
 
 client.run(paw_settings.TOKEN)
